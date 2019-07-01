a5a42d4777e1444f60c4e9aa970c1d3fe299e5e7
zhangyu <zhangyu13393@sina.com>
2019-07-01 22:32:07 +0800

    layout

--- src/components/common/Layout.vue
+++ src/components/common/Layout.vue
@@ -311,7 +311,7 @@ export default {
           );
           this.$router.push({ path: this.activeIndex });
         } else {
-          this.$router.push({ path: "/" });
+          this.$router.push({ path: "/main" });
         }
       }
     }
