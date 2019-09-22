6ac952d049eea034a1ebe9fb4733db3d206a4753
zhangyu <zhangyu13393@sina.com>
2019-09-22 16:39:20 +0800

    design upgrade

--- src/views/test/Test.vue
+++ src/views/test/Test.vue
@@ -8,38 +8,20 @@
                 <el-form-item>
                     <el-button type="warning" icon="el-icon-delete" @click="handleDelete">删除</el-button>
                 </el-form-item>
-                <el-form-item label="测试名称">
+                <el-form-item label="测试名称:">
                     <el-input v-model="testVO.testName" placeholder="测试名称"></el-input>
                 </el-form-item>
-                <el-form-item label="创建时间">
-                    <el-date-picker v-model="createTimeRange" type="datetimerange" start-placeholder="开始时间" end-placeholder="结束时间" :onPick="selectCreateTime()" value-format="timestamp"></el-date-picker>
+                <el-form-item label="创建时间:">
+                    <el-date-picker v-model="createTimeRange" type="datetimerange" start-placeholder="开始时间" end-placeholder="结束时间" :onPick="handleCreateTimeRange()" value-format="timestamp"></el-date-picker>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" icon="el-icon-search" @click="handleSearch()">查询</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
-        <el-table ref="multipleTable" :data="pageBean.data" tooltip-effect="dark" :height="fullHeight" @selection-change="selectToDelete">
+        <el-table ref="multipleTable" :data="pageBean.data" tooltip-effect="dark" :height="fullHeight" @selection-change="selectToDelete" @sort-change="handleSort">
             <el-table-column type="selection" width="42" fixed="left"></el-table-column>
-            <el-table-column type="expand">
-                <template slot-scope="props">
-                    <el-form label-position="left" class="table-expand">
-                        <el-form-item label="创建时间">
-                            <span>{{ props.row.createTime | dateTimeFilter}}</span>
-                        </el-form-item>
-                        <el-form-item label="更新时间">
-                            <span>{{ props.row.updateTime | dateTimeFilter}}</span>
-                        </el-form-item>
-                        <el-form-item label="测试名称">
-                            <span>{{ props.row.testName }}</span>
-                        </el-form-item>
-                        <el-form-item label="测试字典">
-                            <span>{{ props.row.testDictionary | dictionaryFilter(testDictionaryKey)}}</span>
-                        </el-form-item>
-                    </el-form>
-                </template>
-            </el-table-column>
-            <el-table-column prop="createTime" label="创建时间" width="160" :formatter="dateTimeFormatter"></el-table-column>
+            <el-table-column prop="createTime" label="创建时间" width="160" :formatter="dateTimeFormatter" sortable="custom"></el-table-column>
             <el-table-column prop="updateTime" label="更新时间" width="160" :formatter="dateTimeFormatter"></el-table-column>
             <el-table-column prop="testName" label="测试名称" show-overflow-tooltip></el-table-column>
             <el-table-column prop="testDictionary" label="测试字典">
@@ -50,27 +32,28 @@
             <el-table-column fixed="right" label="操作" width="100">
                 <template slot-scope="scope">
                     <el-button @click="handleUpdate(scope.row)" type="text" size="small">编辑</el-button>
+                    <el-button @click="handleDetail(scope.row)" type="text" size="small">详情</el-button>
                 </template>
             </el-table-column>
         </el-table>
         <el-pagination @size-change="handleSizeChange" @current-change="handlePageChange" :page-sizes="[10, 20, 50, 100]" :page-size="testVO.size" layout="->, total, prev, pager, next, jumper, sizes" :total="pageBean.total"></el-pagination>
         <el-dialog title="添加/编辑" :visible.sync="addFormVisible" class="add-form-dialog">
             <el-form :model="test" inline label-width="120px" size="mini">
-                <el-form-item label="金额">
+                <el-form-item label="金额:">
                     <el-input v-model="test.money" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="备注">
+                <el-form-item label="备注:">
                     <el-input type="textarea" v-model="test.remark"></el-input>
                 </el-form-item>
-                <el-form-item label="测试名称">
+                <el-form-item label="测试名称:">
                     <el-input v-model="test.testName" autocomplete="off"></el-input>
                 </el-form-item>
-                <el-form-item label="测试字典">
+                <el-form-item label="测试字典:">
                     <el-select v-model="test.testDictionary" placeholder="请选择">
                         <el-option v-for="item in testDictionaryMap" :key="item.key" :label="item.value" :value="item.key"></el-option>
                     </el-select>
                 </el-form-item>
-                <el-form-item label="创建时间">
+                <el-form-item label="创建时间:">
                     <el-date-picker v-model="createTimePicker" type="datetime" placeholder="请选择创建时间" value-format="timestamp" @change="function(val){test.createTime = val / 1000}"></el-date-picker>
                 </el-form-item>
             </el-form>
@@ -78,6 +61,25 @@
                 <el-button @click="handleClear(false)">取消</el-button>
                 <el-button type="primary" @click="handleAddOrUpdate">确定</el-button>
             </div>
+        </el-dialog>
+        <el-dialog title="详情" :visible.sync="detailFormVisible">
+            <el-form inline label-width="120px" size="mini">
+                <el-form-item label="金额:">
+                    {{test.money}}
+                </el-form-item>
+                <el-form-item label="备注:">
+                    {{test.remark}}
+                </el-form-item>
+                <el-form-item label="测试名称:">
+                    {{test.testName}}
+                </el-form-item>
+                <el-form-item label="测试字典:">
+                    {{test.testDictionary | dictionaryFilter(testDictionaryKey)}}
+                </el-form-item>
+                <el-form-item label="创建时间:">
+                    {{test.createTime | dateTimeFilter}}
+                </el-form-item>
+            </el-form>
         </el-dialog>
     </div>
 </template>
@@ -87,6 +89,7 @@
         data() {
             return {
                 addFormVisible: false,
+                detailFormVisible: false,
                 createTimeRange: [],
                 createTimePicker: null,
                 testVO: {
@@ -98,6 +101,8 @@
                     updateTime: null,
                     updateTimeEnd: null,
                     testName: null,
+                    sortLabel: null,
+                    sortOrder: null,
                 },
                 pageBean: {
                     page: 1,
@@ -133,7 +138,7 @@
                     }
                 });
             },
-            selectCreateTime() {
+            handleCreateTimeRange() {
                 if (this.createTimeRange) {
                     this.testVO.createTime = this.createTimeRange[0] / 1000;
                     this.testVO.createTimeEnd = this.createTimeRange[1] / 1000;
@@ -142,14 +147,22 @@
                     this.testVO.createTimeEnd = null;
                 }
             },
-            handleUpdate(row) {
+            handleSetProperties(row) {
                 this.test.gid = row.gid;
                 this.test.money = row.money;
                 this.test.remark = row.remark;
                 this.test.testName = row.testName;
                 this.test.createTime = row.createTime;
+                this.test.testDictionary = row.testDictionary;
+            },
+            handleUpdate(row) {
+                this.handleSetProperties(row);
                 this.createTimePicker = row.createTime * 1000;
                 this.addFormVisible = true;
+            },
+            handleDetail(row) {
+                this.handleSetProperties(row);
+                this.detailFormVisible = true;
             },
             selectToDelete(val) {
                 let gidList = [];
@@ -197,6 +210,15 @@
                 } else {
                     this.$message.warning("请选择要删除的数据");
                 }
+            },
+            handleSort(data) {
+                switch (data.prop) {
+                    case "createTime":
+                        this.testVO.sortLabel = "create_time";
+                        break;
+                }
+                this.testVO.sortOrder = data.order ? data.order.replace("ending", "") : null;
+                this.handleSearch();
             }
         },
         mounted() {
