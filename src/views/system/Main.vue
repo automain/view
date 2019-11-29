da284b0d33e17fb1f7f0a8ec8e303bb641cb0b5c
zhangyu <zhangyu@weshare.com.cn>
2019-11-29 16:38:22 +0800

    design upgrade

--- src/views/system/Main.vue
+++ src/views/system/Main.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
-        源码地址:https://github.com/automain/automain
+        源码地址(后台部分):https://github.com/automain/automain
+        源码地址(前端部分):https://github.com/automain/view
     </div>
 </template>
